be2b2f0b1375a74566745cf64160c9fba74809d1
Kaiwen Sun <skw_kevin@126.com>
2020-03-21 11:50:51 -0700

    revert readlink

--- translate.sh
+++ translate.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 set -e
-if [ ! -z `which readlink` ]; then
-    pushd $( dirname $( readlink "${BASH_SOURCE[0]}" )) > /dev/null
-else
-    pushd $( dirname "${BASH_SOURCE[0]}" ) > /dev/null
-fi
+pushd $( dirname "${BASH_SOURCE[0]}" ) > /dev/null
 source "scripts/bash_utils.sh"
 if [ ! -d "$VENV" ]; then
     ./scripts/build.sh
